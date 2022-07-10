e2d12182b7ea2e54739958fefc3cc4524fe95884
Michael Herwig <develop@michael-herwig.de>
2022-07-10 12:19:35 +0200

    change test source

--- test/source.cpp
+++ test/source.cpp
@@ -1,36 +1,7 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: (c) 2022, Michael Herwig <contact@michael-herwig.de>
-#define TRACER_OBJ
-#include "tracer/memory.hpp"
-
-#include "tracer/config.hpp"
-
-#include <cassert>
-
-#if defined(TRACER_OS_WINDOWS)
-#include <Windows.h>
-#endif
-
-namespace tracer
+int main()
 {
-
-  void memzero(byte *data, std::size_t datalen)
-  {
-    assert(nullptr != data || 0 == datalen);
-
-    int blob[7];
-    blob[8] = 0;
-
-#if defined(TRACER_OS_WINDOWS)
-    SecureZeroMemory(memadr, memlen);
-#else
-    size_t i;
-    volatile byte *vmemadr = reinterpret_cast<volatile byte *>(data);
-    for (i = 0; i < datalen; ++i)
-    {
-      vmemadr[i] = static_cast<byte>(0);
-    }
-#endif
-  }
-
-} // namespace tracer
+  int rv[7];
+  return rv[8] = 0;
+}
